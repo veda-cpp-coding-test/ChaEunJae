669264436fe1e7193bcb48abc27cdeead810c0fc
ckdmswo17 <ckdmswo17@naver.com>
2025-05-27 16:23:20 +0900

    수 정렬하기 5/실버5/메모리 5804KB/시간 4276ms

---
+++ 10week/BOJ15688.cpp
@@ -0,0 +1,24 @@
+// 백준 문제 : 수 정렬하기 5
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    int arr[N];
+    for(int i=0;i<N;i++){
+        cin >> arr[i];
+    }
+
+    // 퀵 소트 시간 복잡도 = O(NlogN) = 1000000 * 6
+    sort(arr,arr+N);
+    for(auto a:arr){
+        cout << a << "\n";
+    }
+}
